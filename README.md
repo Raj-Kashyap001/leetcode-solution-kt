ae4ad00748f9cbeb2b424e6af4c31f6221b6c526
Raj Kashyap <rajkashyapatservice@outlook.com>
2026-04-08 20:41:43 +0530

    Solved 1.Two Sum Problem

---
+++ README.md
@@ -0,0 +1,5 @@
+# My Solution of problems in leetcode in kotlin
+
+### Current Progress
+
+1. Two Sum. 
---
+++ src/1. Two Sum.kt
@@ -0,0 +1,27 @@
+/**
+ * @Problem 1. Two Sums
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ *
+ * @Input nums = [2,7,11,15], target = 9
+ * @Output [0,1]
+ * @Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ */
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    var result: IntArray = intArrayOf(0, 1)
+    for (i in nums.indices) {
+        for (j in (i + 1)..<nums.size) {
+            if (nums[i] + nums[j] == target) {
+                result = intArrayOf(i, j)
+            }
+        }
+    }
+
+    return result
+}
+
+fun main() {
+    println(twoSum(intArrayOf(3, 2, 4), 6).contentToString())
+}
